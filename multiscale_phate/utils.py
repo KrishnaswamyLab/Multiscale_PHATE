8aa4a5bc0e7086555f79f1a8d43282d868011d5e
Scott Gigante <scottgigante@users.noreply.github.com>
2022-04-13 13:01:18 -0400

    Avoid protocol 5 in python 3.7

--- multiscale_phate/utils.py
+++ multiscale_phate/utils.py
@@ -1,4 +1,15 @@
 import pickle
+import sys
+
+
+def _pickle_protocol():
+    """Get the highest working pickle protocol
+    
+    Pickle protocol 5 is supported on Python 3.7 but doesn't work with loky
+    """
+    if tuple(sys.version.split(".")[:2]) < ('3', '8'):
+        return min(pickle.HIGHEST_PROTOCOL, 4)
+    return pickle.HIGHEST_PROTOCOL
 
 
 def hash_object(X):
@@ -15,4 +26,4 @@ def hash_object(X):
         Description of returned object.
 
     """
-    return hash(pickle.dumps(X, protocol=pickle.HIGHEST_PROTOCOL))
+    return hash(pickle.dumps(X, protocol=_pickle_protocol()))
